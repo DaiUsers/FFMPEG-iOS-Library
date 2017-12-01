8a1eface198f9f0dfec3843f5761aba3ac5404c1
wheng <wheng@yang.local>
2017-12-01 12:06:05 +0800

    Initial Commit

---
+++ ffmpeg-a/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ffmpeg-a
+//
+//  Created by wheng on 2017/12/1.
+//  Copyright © 2017年 wheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
